d36e10c33fb5ce9facd0984d1b6d7025b427f5b0
BabySamurai <51145999+BabySamurai@users.noreply.github.com>
2023-05-24 16:21:07 +0100

    Add files via upload

---
+++ starMicroservices.js
@@ -0,0 +1,37 @@
+const { exec } = require("child_process");
+
+exec("node campgroundMicroservice.js", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+exec("node reviewMicroservice.js", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+exec("node userMicroservice.js", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
